422ea578be8384785621dcc808b20d4427c9a7bf
LunarECL <jkjk98765@naver.com>
2023-02-05 16:45:39 -0500

    feat: setup Main Page.
    changed the name pocker->poker
    add the github workflow

---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <div className="Main">
+      <button className="RoundedButton">Start new Poker</button>
+    </div>
+  );
+};
+
+export default Main;
