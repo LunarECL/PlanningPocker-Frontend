cb0ad2e4054c69a55a63cd79811c644819ca3920
LunarECL <jkjk98765@naver.com>
2023-02-05 21:41:30 -0500

    feat: Game page name popup

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import "./Game.css";
 
 const Game = () => {
   let navigate = useNavigate();
+  const [showPopup, setShowPopup] = useState(false);
   let { id } = useParams();
 
   return (
     <div className="Game">
+      <div>
+        <button onClick={() => setShowPopup(true)}>Show Popup</button>
+        {showPopup && (
+          <div className="overlay">
+            <div className="popup">
+              <h3 className="popup-title">Choose your display name</h3>
+              <input type="text" className="popup-input" required={true} />
+              <span className="label">Name</span>
+              <button className="popup-button">Continue to game</button>
+              <span className="close-popup" onClick={() => setShowPopup(false)}>
+                &times;
+              </span>
+            </div>
+          </div>
+        )}
+      </div>
       <p>Game id: {id}</p>
       <button
         className="RoundedButton"
